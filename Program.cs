fe3214a234d0ca5211bdd64c7f4951733c1ed7f0
AndyLu <laserlue@gmail.com>
2021-09-07 11:11:15 +0800

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NMQ_Model_practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
