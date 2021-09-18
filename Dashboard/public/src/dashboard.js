64cbcc66398484b7c28c9a78fc48911ee9c915b1
AndyLu <laserlue@gmail.com>
2021-09-19 00:40:00 +0800

    completed basic dashboard

--- Dashboard/public/src/dashboard.js
+++ Dashboard/public/src/dashboard.js
@@ -14,8 +14,33 @@ class TaskPlot {
     }
 }
 
-const task1 = new TaskPlot("Task 1", 30, 0);
-const task2 = new TaskPlot("Task 2", 30, 0);
+const task1 = new TaskPlot("Task 1", 0, 0);
+const task2 = new TaskPlot("Task 2", 0, 0);
+
+const publishTask = document.querySelector("#sendMsg");
+const inputTask1 = document.querySelector("#task1-number");
+const inputTask2 = document.querySelector("#task2-number");
+publishTask.addEventListener("click", async () => {
+    insertPlotyBlock(); // init block for ploty
+    if (inputTask1.value < 0) inputTask1.value = 0;
+    else if (inputTask2.value < 0) inputTask2.value = 0;
+    task1.targetNumber = inputTask1.value;
+    task2.targetNumber = inputTask2.value;
+    const inputData = {
+        data: [
+            { type: 1, number: inputTask1.value }, { type: 2, number: inputTask2.value }
+        ]
+    };
+    const data = JSON.stringify(inputData);
+    const response = await fetch("/publishMessage", {
+        body: data,
+        method: "POST",
+        headers: new Headers({
+            "Content-Type": "application/json"
+        })
+    });
+    console.log(response.status);
+});
 
 socket.on("newData", (message) => {
     const task = JSON.parse(message);
@@ -42,20 +67,12 @@ socket.on("newData", (message) => {
     Plotly.newPlot("myDiv", data, layout);
 });
 
-const publishTask = document.querySelector("#send");
-publishTask.addEventListener("click", async () => {
-    const test = {
-        data: [
-            { type: 1, number: 30 }, { type: 2, number: 20 }
-        ]
-    };
-    const data = JSON.stringify(test);
-    const response = await fetch("/publishMessage", {
-        body: data,
-        method: "POST",
-        headers: new Headers({
-            "Content-Type": "application/json"
-        })
-    });
-    console.log(response.status);
-});
+const insertPlotyBlock = () => {
+    const ploty = document.createElement("div");
+    ploty.id = "myDiv";
+    ploty.className = "plot";
+
+    document.querySelector("#init-title").remove();
+    const existingParentElement = document.querySelector("#container");
+    existingParentElement.insertBefore(ploty, existingParentElement.childNodes[0]);
+};
